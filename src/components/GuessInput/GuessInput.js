fad24c308727a3f91cba9c4acb4473fdd543754d
Filipe Yum <85417874+Zentee@users.noreply.github.com>
2024-06-03 21:35:59 +0100

    update: step 5

--- src/components/GuessInput/GuessInput.js
+++ src/components/GuessInput/GuessInput.js
@@ -6,6 +6,8 @@ export default function GuessInput({ answer }) {
   const [textInput, setTextInput] = useState("");
   const [savedInput, setSavedInput] = useState([]);
   const maxGuess = NUM_OF_GUESSES_ALLOWED;
+  let won = false;
+  let isPlaying = false;
 
   function handleSubmit(event) {
     event.preventDefault();
@@ -16,20 +18,46 @@ export default function GuessInput({ answer }) {
     setTextInput("");
   }
 
+  if (savedInput.includes(answer)) {
+    won = true;
+  }
+
+  if (savedInput.length === maxGuess) {
+    isPlaying = true;
+  }
+
   return (
-    <form class="guess-input-wrapper" onSubmit={handleSubmit}>
-      <Guess answer={answer} savedInput={savedInput} />
-      <label for="guess-input">
-        {savedInput.length > maxGuess ? "Enough Guesses." : "Enter guess:"}
-      </label>
-      <input
-        id="guess-input"
-        value={textInput}
-        minLength={5}
-        maxLength={5}
-        onChange={(event) => setTextInput(event.target.value)}
-        type="text"
-      />
-    </form>
+    <>
+      <form class="guess-input-wrapper" onSubmit={handleSubmit}>
+        <Guess answer={answer} savedInput={savedInput} />
+        <label for="guess-input">
+          {savedInput.length > maxGuess ? "Enough Guesses." : "Enter guess:"}
+        </label>
+        <input
+          id="guess-input"
+          value={textInput}
+          minLength={5}
+          maxLength={5}
+          disabled={isPlaying}
+          onChange={(event) => setTextInput(event.target.value)}
+          type="text"
+        />
+      </form>
+      {won && (
+        <div class="happy banner">
+          <p>
+            <strong>Congratulations!</strong> Got it in
+            <strong>3 guesses</strong>.
+          </p>
+        </div>
+      )}
+      {isPlaying && (
+        <div class="sad banner">
+          <p>
+            Sorry, the correct answer is <strong>LEARN</strong>.
+          </p>
+        </div>
+      )}
+    </>
   );
 }
